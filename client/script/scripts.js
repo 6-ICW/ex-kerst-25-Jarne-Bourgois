5c61b59db2a66fda7d0dc95a12efa2d278d77217
jarnebourgois <Jarne.Bourgois@student.tsaam.be>
2025-12-09 10:14:39 +0100

    commit laatste

--- client/script/scripts.js
+++ client/script/scripts.js
@@ -1,6 +1,12 @@
+// vraag 1
 const text = document.querySelector("#text");
 const password = document.querySelector("#password");
 const btnlogin = document.querySelector("#btnlogin");
+// vraag 2
+currentPSW = document.querySelector("#currentPSW");
+newPSW = document.querySelector("#newPSW");
+ConfirmNewPSW = document.querySelector("#ConfirmNewPSW");
+btnUpdate = document.querySelector("#btnUpdate");
 
 btnlogin.addEventListener("click", (e) => {
   console.log(text);
@@ -15,6 +21,24 @@ btnlogin.addEventListener("click", (e) => {
     body: JSON.stringify({
       login: text.value,
       password: password.value,
+    }),
+  };
+  fetch("https://ex-kerst-2025.onrender.com/user/", options)
+    .then((result) => result.json())
+    .then((data) => {
+      console.log(data);
+    });
+});
+
+btnUpdate.addEventListener("click", (e) => {
+  options = {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: 1,
+      password: newPSW.value,
     }),
   };
   fetch("https://ex-kerst-2025.onrender.com/user/", options)
