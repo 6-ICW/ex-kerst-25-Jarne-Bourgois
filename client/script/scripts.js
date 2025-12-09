063624e112f78305d5c75050e5a9e4b713ec5400
jarnebourgois <Jarne.Bourgois@student.tsaam.be>
2025-12-09 10:04:08 +0100

    commit 2

--- client/script/scripts.js
+++ client/script/scripts.js
@@ -1,9 +1,25 @@
-const email = document.querySelector("#email");
+const text = document.querySelector("#text");
 const password = document.querySelector("#password");
 const btnlogin = document.querySelector("#btnlogin");
 
-
-
-btnlogin.addEventListener("click",()=>{
-    
-})
+btnlogin.addEventListener("click", (e) => {
+  console.log(text);
+  console.log(text.value);
+  console.log(password);
+  console.log(password.value);
+  options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      login: text.value,
+      password: password.value,
+    }),
+  };
+  fetch("https://ex-kerst-2025.onrender.com/user/", options)
+    .then((result) => result.json())
+    .then((data) => {
+      console.log(data);
+    });
+});
